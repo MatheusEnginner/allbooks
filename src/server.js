f3a3e921e93ef3993ad97b70ec8573f820da8d69
MateusEnginner <mateusantos1608@hotmail.com>
2024-12-16 19:53:05 -0300

    Altera o server

--- src/server.js
+++ src/server.js
@@ -19,5 +19,5 @@ server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
   console.log("Boas-vindas a API do Allbooks")
-  console.log("API disponivel através da url http://localhost:8000")
+  console.log("API disponivel em http://localhost:8000")
 })
